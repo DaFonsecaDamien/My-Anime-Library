10f265e3512786d2a22a2cafdb1a0f3fa0d374c7
Ennasiri Mehdi <ennasirimehdi@gmail.com>
2022-05-30 22:44:17 +0200

    fix object

--- src/main/java/com/example/myanimelibrary/domain/UserManga.java
+++ src/main/java/com/example/myanimelibrary/domain/UserManga.java
@@ -1,153 +1,58 @@
 package com.example.myanimelibrary.domain;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Map;
-
 public class UserManga {
 
-    private String id;
-    private Map<String, String> titles;
-    private String imageUrl;
-    private LocalDate startDate;
-    private LocalDate endDate;
-    private Integer chapters;
-    private Integer volumes;
-    private String author;
-    private String synopsys;
-    private String type;
-    private MangaState state;
-    private ArrayList<String> genre;
-    private ArrayList<Score> scores;
-    private Integer rank;
+    private Manga manga;
+    private User user;
+    private Integer currentChapter;
+    private Integer score;
+    private String comment;
 
-    public UserManga(String id, Map<String, String> titles, String imageUrl, LocalDate startDate, LocalDate endDate, Integer chapters, Integer volumes, String author, String synopsys, String type, MangaState state, ArrayList<String> genre, ArrayList<Score> scores, Integer rank) {
-        this.id = id;
-        this.titles = titles;
-        this.imageUrl = imageUrl;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.chapters = chapters;
-        this.volumes = volumes;
-        this.author = author;
-        this.synopsys = synopsys;
-        this.type = type;
-        this.state = state;
-        this.genre = genre;
-        this.scores = scores;
-        this.rank = rank;
+    public UserManga(Manga manga, User user, Integer currentChapter, Integer score, String comment) {
+        this.manga = manga;
+        this.user = user;
+        this.currentChapter = currentChapter;
+        this.score = score;
+        this.comment = comment;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setManga(Manga manga) {
+        this.manga = manga;
     }
 
-    public void setTitles(Map<String, String> titles) {
-        this.titles = titles;
+    public void setUser(User user) {
+        this.user = user;
     }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
+    public void setCurrentChapter(Integer currentChapter) {
+        this.currentChapter = currentChapter;
     }
 
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
+    public void setScore(Integer score) {
+        this.score = score;
     }
 
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
+    public void setComment(String comment) {
+        this.comment = comment;
     }
 
-    public void setChapters(Integer chapters) {
-        this.chapters = chapters;
+    public Manga getManga() {
+        return manga;
     }
 
-    public void setVolumes(Integer volumes) {
-        this.volumes = volumes;
+    public User getUser() {
+        return user;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
+    public Integer getCurrentChapter() {
+        return currentChapter;
     }
 
-    public void setSynopsys(String synopsys) {
-        this.synopsys = synopsys;
+    public Integer getScore() {
+        return score;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public void setState(MangaState state) {
-        this.state = state;
-    }
-
-    public void setGenre(ArrayList<String> genre) {
-        this.genre = genre;
-    }
-
-    public void setScores(ArrayList<Score> scores) {
-        this.scores = scores;
-    }
-
-    public void setRank(Integer rank) {
-        this.rank = rank;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public Map<String, String> getTitles() {
-        return titles;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public Integer getChapters() {
-        return chapters;
-    }
-
-    public Integer getVolumes() {
-        return volumes;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getSynopsys() {
-        return synopsys;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public MangaState getState() {
-        return state;
-    }
-
-    public ArrayList<String> getGenre() {
-        return genre;
-    }
-
-    public ArrayList<Score> getScores() {
-        return scores;
-    }
-
-    public Integer getRank() {
-        return rank;
+    public String getComment() {
+        return comment;
     }
 }
-
