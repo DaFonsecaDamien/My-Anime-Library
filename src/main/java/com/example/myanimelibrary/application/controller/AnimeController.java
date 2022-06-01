0ef184892f416beedd9489bd437178eb98f50091
Ennasiri Mehdi <ennasirimehdi@gmail.com>
2022-06-01 17:24:13 +0200

    template fix

--- src/main/java/com/example/myanimelibrary/application/controller/AnimeController.java
+++ src/main/java/com/example/myanimelibrary/application/controller/AnimeController.java
@@ -1,9 +1,16 @@
 package com.example.myanimelibrary.application.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/animeList/anime")
 public class AnimeController {
+
+    @GetMapping("/add")
+    public String addBook() {
+        return "bookServicePort.addBook(bookDto)";
+    }
 }
