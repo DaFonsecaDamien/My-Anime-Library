a2dae948dfd641c4a4a8d784132ef9be26e34def
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2022-06-01 16:57:25 +0200

    [FIX] CI on dev

--- src/test/java/com/example/myanimelibrary/MyAnimeLibraryApplicationTests.java
+++ src/test/java/com/example/myanimelibrary/MyAnimeLibraryApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.myanimelibrary;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MyAnimeLibraryApplicationTests.class)
 class MyAnimeLibraryApplicationTests {
 
     @Test
