c3b7614cd9096e1c316c25e41e0fc2a059a93bc7
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2022-07-14 15:27:11 +0200

    [ADD] Controller routes

--- src/main/java/com/example/myanimelibrary/infrastructure/web/controller/UserController.java
+++ src/main/java/com/example/myanimelibrary/infrastructure/web/controller/UserController.java
@@ -1,15 +1,35 @@
 package com.example.myanimelibrary.infrastructure.web.controller;
 
+import com.example.myanimelibrary.domain.User;
 import com.example.myanimelibrary.domain.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
 
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.saveUser(user));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable("username") String username) {
+        return ResponseEntity.ok(userService.getUserByUsername(username));
     }
 }
